b2c92e0f8f9fcb2f2393e9fd59f1db54013c78f6
Julian Villalba <62047235+jrvillalba1993@users.noreply.github.com>
2020-08-25 15:10:08 -0400

    added wallpaper and changed button positions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,14 +3,18 @@ import Header from './Header';
 import Books from './Books';
 import logo from './logo.svg';
 import './App.css';
+import wallpaper from  './images/white_leather.png';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" styles={{ backgroundImage:`url(${wallpaper})`}}>
         <Header />
         <Books />
+        
       </div>
+
+
     );
   }
   
